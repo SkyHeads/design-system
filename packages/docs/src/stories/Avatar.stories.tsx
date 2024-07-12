e5ee5422b9e03785a6047485fd6aa24d4ca8e8ac
Bruno <skyheads@live.com>
2024-07-12 03:37:19 -0300

    refactor: argtypes storybook

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/SkyHeads.png',
     alt: 'SkyHeads',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
